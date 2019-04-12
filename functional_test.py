62bb745e9687aa6326236fe287b14d3d176ffd58
Rashid Omar <amidurashid@gmail.com>
2019-04-12 01:14:49 +0000

    Added list which also failes the unit test

--- functional_test.py
+++ functional_test.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 browser = webdriver.Firefox()
 
-browser.get("http://localhost:8001")
+browser.get("http://localhost:8000")
 
-assert "Django" in browser.title
+assert 'To-Do' in browser.title
 
