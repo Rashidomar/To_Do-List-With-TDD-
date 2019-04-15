71787df13105b5b943f4cd339658cda91a1694cf
Rashid Omar <amidurashid@gmail.com>
2019-04-15 23:23:21 +0000

    The test for the hompage failed and later passed successfully and the basic view now returns minial html content

--- lists/tests.py
+++ lists/tests.py
@@ -1,13 +1,24 @@
 from django.test import TestCase
 from lists.views import homepage
 from django.urls import resolve
-#from django.http import HttpRequest
+from django.http import HttpRequest
 
 # Create your tests here.
 
 class HomePageTest(TestCase):
-    def test_homepage(self):
+    def test_url(self):
         found = resolve('/')
         self.assertEqual(found.func, homepage)
+
+    # testing for homepage and it should correct html 
+    def test_homepage(self):
+        request = HttpRequest()
+        response = homepage(request)
+        html = response.content.decode()
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
+
+    
     
    
